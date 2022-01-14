79d01908fb53c4fbec40d634151edfc77cefa279
Sajda Kabir <sajda.kbir@gmail.com>
2022-01-14 00:18:39 -0800

    add dark mode

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import TextFrom from './components/TextFrom';
+import React, { useState } from "react";
 
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextFrom from "./components/TextFrom";
 
 function App() {
-  return (
-   
-<>
-<Navbar title="Textutils" aboutText="About Us"/>
-<div className="container my-3">
-<TextFrom heading="Enter the text to analyze below"/>
-</div>
+  const [mode, setMode] = useState("light");
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
 
-</>
- 
+      // document.body.style.backgroundColor='#0E0D5D';
+      document.body.style.backgroundColor = "#0C0032";
+    } else {
+      setMode("light");
+
+      document.body.style.backgroundColor = "white";
+    }
+  };
+
+  return (
+    <>
+      <Navbar
+        title="Textutils"
+        aboutText="About Us"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+      <div className="container my-3">
+        <TextFrom heading="Enter the text to analyze below" mode={mode} />
+      </div>
+    </>
   );
 }
 
--- src/components/TextFrom.js
+++ src/components/TextFrom.js
@@ -1,63 +1,97 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
 export default function TextFrom(props) {
-    function handleChange(e){
-        setText(e.target.value);
+  function handleChange(e) {
+    setText(e.target.value);
+  }
+  function handleUpClick() {
+    const newText = text.toUpperCase();
+    setText(newText);
+  }
+  function handleLwClick() {
+    const newText = text.toLowerCase();
+    setText(newText);
+  }
+  function handleClClick() {
+    const newText = "";
+    setText(newText);
+  }
+  function handleCpClick() {
+    const text = document.getElementById("myBox");
+    text.select();
 
-    }
-    function handleUpClick(){
-        const newText=text.toUpperCase();
-        setText(newText);
-    }
-    function handleLwClick(){
-        const newText=text.toLowerCase();
-        setText(newText);
-    }
-    function handleClClick(){
-        const newText='';
-        setText(newText);
-    }
-    function handleCpClick(){
-        const text=document.getElementById("myBox")
-        text.select();
-    //    console.log(text.value);
-        navigator.clipboard.writeText(text.value);
-       
-    }
-   const handleExtraSpace=()=>{
-   
-    const newText=text.split(/[ ]+/);
+    navigator.clipboard.writeText(text.value);
+  }
+  const handleExtraSpace = () => {
+    const newText = text.split(/[ ]+/);
     setText(newText.join(" "));
-    }
-    const handleCapClick=()=>{
-      const newText= text .split(' ')
-        .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
-        .join(' ');
-        setText(newText);
-    }
-    const [text,setText]=useState("")
-    return (
-        <>
-        <div className='container'>
-            <h1>{props.heading}</h1>
-    
-            <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleChange} id="myBox" rows="8"></textarea>
-            <button  className="btn btn-dark my-3" onClick={handleUpClick}>Convert to Uppercase</button>
-            <button  className="btn btn-dark my-3 mx-2" onClick={handleLwClick}>Convert to Lowercase</button>
-            <button  className="btn btn-dark my-3 mx-2" onClick={handleCapClick}>Convert to Capitalize Case</button>
-            <button  className="btn btn-dark my-3 mx-2" onClick={handleCpClick}>Copy text</button>
-            <button  className="btn btn-dark my-3 mx-2" onClick={handleExtraSpace}>Remove Extra Space</button>
-            <button  className="btn btn-dark my-3 mx-2" onClick={handleClClick}>Clear text</button>
-            </div>
+  };
+  const handleCapClick = () => {
+    const newText = text
+      .split(" ")
+      .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
+      .join(" ");
+    setText(newText);
+  };
+  const [text, setText] = useState("");
+  return (
+    <>
+      <div
+        className="container"
+        // style={{ color: props.mode === "dark" ? "white" : "dark" }}
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        <h1>{props.heading}</h1>
+
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleChange}
+           
+            // style={{
+            //   backgroundColour: props.mode === "dark" ? "#0C0032" : "white",
+            //   color: props.mode === "dark" ? "white" : "black",
+            // }}
+            style={{
+              backgroundColor: props.mode === "dark" ? "#0C0032" : "white",
+              color: props.mode === "dark" ? "white" : "black",
+            }}
+            id="myBox"
+            rows="8"
+          ></textarea>
+          <button className="btn btn-dark my-3" onClick={handleUpClick}>
+            Convert to Uppercase
+          </button>
+          <button className="btn btn-dark my-3 mx-2" onClick={handleLwClick}>
+            Convert to Lowercase
+          </button>
+          <button className="btn btn-dark my-3 mx-2" onClick={handleCapClick}>
+            Convert to Capitalize Case
+          </button>
+          <button className="btn btn-dark my-3 mx-2" onClick={handleCpClick}>
+            Copy text
+          </button>
+          <button className="btn btn-dark my-3 mx-2" onClick={handleExtraSpace}>
+            Remove Extra Space
+          </button>
+          <button className="btn btn-dark my-3 mx-2" onClick={handleClClick}>
+            Clear text
+          </button>
         </div>
-        <div className="container">
-            <h1>Your text summery</h1>
-            <p>{text.split(" ").length-1} Words and {text.length} Characters</p>
-            <p>{0.008*(text.split(" ").length-1)} Minutes to read</p>
-            <h2>Preview</h2>
-            <p>{text}</p>
-        </div>
-        </>
-    )
+      </div>
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        <h1>Your text summery</h1>
+        <p>
+          {text.split(" ").length - 1} Words and {text.length} Characters
+        </p>
+        <p>{0.008 * (text.split(" ").length - 1)} Minutes to read</p>
+        <h2>Preview</h2>
+        <p>{text.length > 0 ? text : "write somthing to see preview"}</p>
+      </div>
+    </>
+  );
 }
